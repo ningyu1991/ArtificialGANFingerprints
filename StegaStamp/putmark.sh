68ce1521e1a85eef9f44d344d72b599871e786b7
Vladislav Skripniuk <vladislav@d2volta15.mpi-inf.mpg.de>
2021-01-10 19:37:23 +0100

    Remove Stegastamp dir.

--- StegaStamp/putmark.sh
+++
@@ -1,9 +0,0 @@
-python putmark.py --model stegastamp --dataset CelebA --modelpath "../saved_models/celeba_" \
-    --datasetpath 'celeba/path' --savedir "pth/stegastamp_CelebA" --batchsize 50 --cuda '2' \
-    --output_size 100 --secret_size 100 
-
-python putmark.py --model stegastamp --dataset LSUN --modelpath "../saved_models/lsun_" \
-    --datasetpath 'LSUN/bedrooms/path' --savedir "pth/stegastamp_LSUN" --batchsize 50 --cuda '2' \
-    --output_size 100 --secret_size 100
-
-
--- models.py
+++ models.py
@@ -2,45 +2,52 @@ import torch
 from torch import nn
 from torch.nn.functional import relu, sigmoid
 
+
 class StegaStampEncoder(nn.Module):
-    def __init__(self, height=32, width=32, IMAGE_CHANNELS=1, secret_size=100, return_residual=False):
+    def __init__(
+        self,
+        height=32,
+        width=32,
+        IMAGE_CHANNELS=1,
+        secret_size=100,
+        return_residual=False,
+    ):
         super(StegaStampEncoder, self).__init__()
         self.secret_size = secret_size
         self.IMAGE_CHANNELS = IMAGE_CHANNELS
         self.return_residual = return_residual
-        self.secret_dense = nn.Linear(self.secret_size, 16*16*IMAGE_CHANNELS)
+        self.secret_dense = nn.Linear(self.secret_size, 16 * 16 * IMAGE_CHANNELS)
         if height == width == 32:
-            self.secret_upsample = nn.Upsample(scale_factor=(2,2))
+            self.secret_upsample = nn.Upsample(scale_factor=(2, 2))
         elif height == width == 128:
-            self.secret_upsample = nn.Upsample(scale_factor=(8,8))
+            self.secret_upsample = nn.Upsample(scale_factor=(8, 8))
         elif height == width == 512:
-            self.secret_upsample = nn.Upsample(scale_factor=(32,32))
+            self.secret_upsample = nn.Upsample(scale_factor=(32, 32))
         else:
             raise NotImplementedError
-        self.conv1 = nn.Conv2d(2*IMAGE_CHANNELS, 32, 3, 1, 1)
+        self.conv1 = nn.Conv2d(2 * IMAGE_CHANNELS, 32, 3, 1, 1)
         self.conv2 = nn.Conv2d(32, 32, 3, 2, 1)
         self.conv3 = nn.Conv2d(32, 64, 3, 2, 1)
         self.conv4 = nn.Conv2d(64, 128, 3, 2, 1)
         self.conv5 = nn.Conv2d(128, 256, 3, 2, 1)
         self.pad6 = nn.ZeroPad2d((0, 1, 0, 1))
         self.up6 = nn.Conv2d(256, 128, 2, 1)
-        self.upsample6 = nn.Upsample(scale_factor=(2,2))
-        self.conv6 = nn.Conv2d(128+128, 128, 3, 1, 1)
+        self.upsample6 = nn.Upsample(scale_factor=(2, 2))
+        self.conv6 = nn.Conv2d(128 + 128, 128, 3, 1, 1)
         self.pad7 = nn.ZeroPad2d((0, 1, 0, 1))
         self.up7 = nn.Conv2d(128, 64, 2, 1)
-        self.upsample7 = nn.Upsample(scale_factor=(2,2))
-        self.conv7 = nn.Conv2d(64+64, 64, 3, 1, 1)
+        self.upsample7 = nn.Upsample(scale_factor=(2, 2))
+        self.conv7 = nn.Conv2d(64 + 64, 64, 3, 1, 1)
         self.pad8 = nn.ZeroPad2d((0, 1, 0, 1))
         self.up8 = nn.Conv2d(64, 32, 2, 1)
-        self.upsample8 = nn.Upsample(scale_factor=(2,2))
-        self.conv8 = nn.Conv2d(32+32, 32, 3, 1, 1)
+        self.upsample8 = nn.Upsample(scale_factor=(2, 2))
+        self.conv8 = nn.Conv2d(32 + 32, 32, 3, 1, 1)
         self.pad9 = nn.ZeroPad2d((0, 1, 0, 1))
         self.up9 = nn.Conv2d(32, 32, 2, 1)
-        self.upsample9 = nn.Upsample(scale_factor=(2,2))
-        self.conv9 = nn.Conv2d(32+32+2*IMAGE_CHANNELS, 32, 3, 1, 1)
+        self.upsample9 = nn.Upsample(scale_factor=(2, 2))
+        self.conv9 = nn.Conv2d(32 + 32 + 2 * IMAGE_CHANNELS, 32, 3, 1, 1)
         self.conv10 = nn.Conv2d(32, 32, 3, 1, 1)
         self.residual = nn.Conv2d(32, IMAGE_CHANNELS, 1)
-
 
     def forward(self, secret, image):
         secret = relu(self.secret_dense(secret))
@@ -70,6 +77,7 @@ class StegaStampEncoder(nn.Module):
             residual = sigmoid(residual)
         return residual
 
+
 class StegaStampDecoder(nn.Module):
     def __init__(self, height=32, width=32, IMAGE_CHANNELS=1, secret_size=1):
         super(StegaStampDecoder, self).__init__()
@@ -77,31 +85,32 @@ class StegaStampDecoder(nn.Module):
         self.width = width
         self.IMAGE_CHANNELS = IMAGE_CHANNELS
         self.decoder = nn.Sequential(
-            nn.Conv2d(IMAGE_CHANNELS, 32, (3, 3), 2, 1), #16
+            nn.Conv2d(IMAGE_CHANNELS, 32, (3, 3), 2, 1),  # 16
             nn.ReLU(),
             nn.Conv2d(32, 32, 3, 1, 1),
             nn.ReLU(),
-            nn.Conv2d(32, 64, 3, 2, 1), #8
+            nn.Conv2d(32, 64, 3, 2, 1),  # 8
             nn.ReLU(),
             nn.Conv2d(64, 64, 3, 1, 1),
             nn.ReLU(),
-            nn.Conv2d(64, 64, 3, 2, 1), #4
+            nn.Conv2d(64, 64, 3, 2, 1),  # 4
             nn.ReLU(),
-            nn.Conv2d(64, 128, 3, 2, 1), #2
+            nn.Conv2d(64, 128, 3, 2, 1),  # 2
             nn.ReLU(),
             nn.Conv2d(128, 128, (3, 3), 2, 1),
             nn.ReLU(),
         )
         self.dense = nn.Sequential(
-            nn.Linear(height*width*128//32//32, 512),
+            nn.Linear(height * width * 128 // 32 // 32, 512),
             nn.ReLU(),
-            nn.Linear(512, secret_size)
+            nn.Linear(512, secret_size),
         )
 
     def forward(self, image):
         x = self.decoder(image)
-        x = x.view(-1,self.height*self.width*128//32//32)
+        x = x.view(-1, self.height * self.width * 128 // 32 // 32)
         return self.dense(x)
+
 
 class Discriminator(nn.Module):
     def __init__(self, height, width, IMAGE_CHANNELS):
@@ -115,11 +124,10 @@ class Discriminator(nn.Module):
             nn.ReLU(),
             nn.Conv2d(32, 64, (3, 3), 2, 1),
             nn.ReLU(),
-            nn.Conv2d(64, 1, (2, 2), 1)
+            nn.Conv2d(64, 1, (2, 2), 1),
         )
 
     def forward(self, image):
-            x = self.model(image)
-            output = torch.mean(x, dim=[1, 2, 3]).view(-1, 1)
-            return output, x
-
+        x = self.model(image)
+        output = torch.mean(x, dim=[1, 2, 3]).view(-1, 1)
+        return output, x
